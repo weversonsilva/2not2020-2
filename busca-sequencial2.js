b5ea0d564c09c362b03e4e218b3b4f79d2e6e2b7
weversonsilva <wevfca@hotmail.com>
2020-09-03 23:29:57 +0000

    (03/09) Busca Sequencial

---
+++ busca-sequencial2.js
@@ -0,0 +1,27 @@
+function buscaSequencial(lista, valorBusca, fnComp) {
+    for(let i = 0; i < lista.length; i++) {
+        // Encontrou o que está buscando: retorna a posição em que foi encontrado
+        //if(lista[i] === valorBusca) return i
+        if(fnComp(lista[i], valorBusca)) return i
+    }
+    return -1 // valorBusca não foi encontrado em lista
+}
+
+const listaNomes = require('./dados/lista-nomes')
+
+function comparaNome(obj, nome) {
+// Retorna true se o atributo first_name for igual a nome
+    return obj.first_name === nome
+}    
+
+console.time('Weverson')
+console.log(buscaSequencial(listaNomes, 'WEVERSON', comparaNome))
+console.timeEnd('Weverson')
+
+console.log('----------------------------------')
+
+console.time('10000')
+// Encontrar o primeiro nome da lista que possui mais de 10000 pessoas registradas com ele
+let pos = buscaSequencial(listaNomes, 10000, (obj, valor) => obj.frequency_total > valor)
+console.log(listaNomes[pos], pos)
+console.timeEnd('10000')
