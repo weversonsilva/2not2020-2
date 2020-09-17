2954c27b92ea04a7953689fd93f3ee4e83fbd4cc
weversonsilva <wevfca@hotmail.com>
2020-09-17 23:21:24 +0000

    (17/09) Algoritmo ordenação bubblesort com função de comparação

--- bubble-sort_objeto.js
+++ bubble-sort_objeto.js
@@ -1,3 +1,12 @@
+/*
+    Adaptando o algoritmo BUBBLE SORT para operar com vetor de objetos
+    1) Acrescentar um novo parâmetro, que vai corresponder à função de comparação (fnComp)
+    2) Essa função será chamada de dentro do bubbleSort(), recebendo o elemento  da posição
+    atual e da posição seguinte como parâmetros.
+    Ela deve retornar true caso o primeiro parâmetro seja maior que o segundo,
+    ou false caso contrário.
+*/
+
 /* Algoritmo de ordenação Bubble Sort
 
 Estratégia
@@ -8,7 +17,7 @@ O percurso no vetor é feito tantas vezes quando necessário, até
 que nenhuma troca seja efetuada no percurso.
 */
 
-function bubbleSort(vetor) {
+function bubbleSort(vetor, fnComp) {
     let passadas = 0, comparacoes = 0, totalTrocas = 0
     let trocas
     do {
@@ -20,7 +29,8 @@ function bubbleSort(vetor) {
             comparacoes++
             // Comparando o elemento da posição atual [i]
             // com o elemento da frente (i + 1)
-            if(vetor[i] > vetor[i+1]) {
+            //if(vetor[i] > vetor[i+1]) {
+            if(fnComp(vetor[i], vetor[i+1])) {    
                 // Troca usando desestruturação de vetor
                 [vetor[i], vetor[i+1]] = [vetor[i+1], vetor[i]]
                 trocas++
@@ -32,27 +42,25 @@ function bubbleSort(vetor) {
     console.log({passadas, comparacoes, totalTrocas})
 }
 
+const candidatos = require('./dados/candidatos-2018')
+/*
+console.time('Teste candidatos')
+bubbleSort(candidatos, (a, b) => a.NR_CANDIDATO > b.NR_CANDIDATO)
+console.timeEnd('Teste candidatos')
 
-let nums = [58, 16, 33, 82, 4, 47, 25, 71, 96, 60, 41, 89]
+console.log(candidatos)
+*/
 
-console.time('Teste 1')
-bubbleSort(nums)
-console.timeEnd('Teste 1')
+// Fazendo um map() para produzir um novo vetor só com os nomes dos candidatos
+const nomeCandidatos = candidatos.map(o => o.NM_CANDIDATO)
+console.log(nomeCandidatos)
 
-console.log(nums)
+// Teste com localeCompare()
+// Fatiando 1000 primeiros candidatos
+// localeCompare() faz corretamente a ordenação considerando os acentos mas é muito demorado
+const candidatos1000 = candidatos.slice(0, 999)
+console.time('Teste candidatos 1000')
+bubbleSort(candidatos1000, (a, b) => a.NM_URNA_CANDIDATO.localeCompare(b.NM_URNA_CANDIDATO, 'pt-BR')>0)
+console.timeEnd('Teste candidatos 1000')
 
-let listaEmpresas = require('./dados/15-mil-empresas')
-
-console.time('Empresas')
-bubbleSort(listaEmpresas)
-console.timeEnd('Empresas')
-
-console.log(listaEmpresas)
-
-let listaNomes = require('./dados/100-mil-nomes')
-
-console.time('Nomes')
-bubbleSort(listaNomes)
-console.timeEnd('Nomes')
-
-console.log(listaNomes)
+console.log(candidatos1000)
