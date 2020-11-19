75724fd58d5c6fcf3309284723829c5477ce5ffa
weversonsilva <wevfca@hotmail.com>
2020-11-19 23:22:05 +0000

    Arvore 19/11/2020

---
+++ teste-bst.js
@@ -0,0 +1,19 @@
+const BinarySearchTree = require('./lib/BinarySearchTree')
+
+let arvore = new BinarySearchTree
+console.log(arvore)
+
+arvore.insert(33)
+console.log(arvore)
+
+arvore.insert(59)
+console.log(arvore)
+
+arvore.insert(10)
+console.log(arvore)
+
+arvore.insert(25)
+console.log(arvore)
+
+arvore.insert(41)
+console.log(arvore)
