e81acdd0f2a68d2267378247096afc95f9ca32ff
weversonsilva <wevfca@hotmail.com>
2020-11-26 23:33:42 +0000

    Ordem, Pré e Pós

--- teste-bst.js
+++ teste-bst.js
@@ -12,8 +12,32 @@ console.log(arvore)
 arvore.insert(10)
 console.log(arvore)
 
-arvore.insert(25)
-console.log(arvore)
-
 arvore.insert(41)
 console.log(arvore)
+
+arvore.insert(6)
+arvore.insert(70)
+arvore.insert(27)
+arvore.insert(8)
+arvore.insert(64)
+console.log(arvore)
+
+//arvore.inOrderTraversal(x => console.log(x)) // Em ordem
+
+let vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+vet = []
+arvore.preOrderTraversal(d => vet.push(d))
+console.log('Pré-ordem:', vet)
+
+vet = []
+arvore.postOrderTraversal(d => vet.push(d))
+console.log('Pós-ordem:', vet)
+
+// Menor valor da árvore
+console.log('Mínimo: ', arvore.min())
+
+// Maior valor da árvore
+console.log('Máximo: ', arvore.max())
