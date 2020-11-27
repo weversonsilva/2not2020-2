da4ff3c081e5edd4f2ec8465f74cf94e9e6ebdc4
weversonsilva <wevfca@hotmail.com>
2020-11-27 23:31:21 +0000

    Ultima

--- teste-bst.js
+++ teste-bst.js
@@ -41,3 +41,44 @@ console.log('Mínimo: ', arvore.min())
 
 // Maior valor da árvore
 console.log('Máximo: ', arvore.max())
+
+console.log('Existe 41?', arvore.search(41))
+console.log('Existe 8?', arvore.search(8))
+console.log('Existe 59?', arvore.search(59))
+console.log('Existe 20?', arvore.search(20))
+console.log('Existe 51?', arvore.search(51))
+
+arvore.insert(35)
+arvore.insert(39)
+arvore.insert(34)
+
+vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+// Caso 1: Exclusão de um nodo folha (grau 0)
+arvore.remove(39)
+vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+// Caso 2a: Exclusão de um nod grau 1 com subarvore a esquerda
+arvore.remove(70)
+vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+// Caso 2b: Exclusão de um nodo grau 1 com subarvore a direita
+arvore.remove(6)
+vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+// Caso 3: Exclusão de um nodo grau 2
+arvore.remove(59)
+vet = []
+arvore.inOrderTraversal(d => vet.push(d))
+console.log('Em-ordem:', vet)
+
+
+
